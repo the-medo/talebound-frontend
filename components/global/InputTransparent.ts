aa530c8e4da7fcf887f72fe5a87195f6d6ee4f70
Medo <martinmederly@gmail.com>
2023-04-30 17:24:01 +0200

    feat: extend nextui input to use it in registration and login forms

--- components/global/InputTransparent.ts
+++ components/global/InputTransparent.ts
@@ -1,26 +1,43 @@
-import {styled} from "@nextui-org/react";
+import {CSS, Input, styled} from "@nextui-org/react";
 
-export const InputTransparent = styled('input', {
-  fontFamily: '$heading',
-  padding: '$5',
-  border: 'none',
-  borderRadius: '$md',
+// export const InputTransparent = styled('input', {
+//   fontFamily: '$heading',
+//   padding: '$5',
+//   border: 'none',
+//   borderRadius: '$md',
+//   background: '$transparent40',
+//   color: '$primary800',
+//   fontSize: '$md',
+//   fontWeight: '$bold',
+//   textDecoration: 'none',
+//   cursor: 'pointer',
+//   transition: 'all 0.2s ease-in-out',
+//   width: '$full',
+//
+//   '&:hover': {
+//     background: '$transparent50',
+//   },
+//
+//   '&:focus': {
+//     outline: 'none',
+//     background: '$transparent50',
+//     boxShadow: '$focus',
+//   }
+// });
+
+
+const InputProps: CSS = {
   background: '$transparent40',
-  color: '$primary800',
-  fontSize: '$md',
-  fontWeight: '$bold',
-  textDecoration: 'none',
+  fontFamily: '$heading',
+  paddingRight: '$5',
+  borderRadius: '$md',
   cursor: 'pointer',
   transition: 'all 0.2s ease-in-out',
-  width: '$full',
 
-  '&:hover': {
-    background: '$transparent50',
+  '.nextui-input-wrapper': {
+    background: 'transparent',
   },
+};
 
-  '&:focus': {
-    outline: 'none',
-    background: '$transparent50',
-    boxShadow: '$focus',
-  }
-});
+export const InputTransparent = styled(Input, InputProps);
+export const PasswordTransparent = styled(Input.Password, InputProps);
--- components/homepage/Register.tsx
+++ components/homepage/Register.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import {styled} from "@nextui-org/react";
+import React, {useMemo} from "react";
+import {styled, Input, useInput, InputProps} from "@nextui-org/react";
 import {VerticalSemitransparent} from "../global/VerticalSemitransparent";
 import {Button} from "../global/Button";
-import {InputTransparent} from "../global/InputTransparent";
+import {InputTransparent, PasswordTransparent} from "../global/InputTransparent";
 
 
 const RegisterBackground = styled('div', {
@@ -17,7 +17,7 @@ const RegisterBackground = styled('div', {
 });
 
 const RegisterBox = styled(VerticalSemitransparent, {
-  gap: '$xs',
+  gap: '$md',
   position: 'absolute',
   top: '$0',
   bottom: '$0',
@@ -36,6 +36,12 @@ const RegisterBox = styled(VerticalSemitransparent, {
     right: '$0',
     transform: 'translate(0%, 0%)',
   },
+  '& .nextui-input-helper-text-container': {
+    width: '$full',
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+
 });
 
 const RegisterHeader = styled('h2', {
@@ -45,9 +51,15 @@ const RegisterHeader = styled('h2', {
 const RegisterLabel = styled('label', {
   color: '$primary800',
   fontFamily: '$heading',
-  width: '100%',
   fontSize: '$md',
   textTransform: 'uppercase',
+});
+
+const FieldWrapper = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '$full',
+  marginBottom: '$md',
 });
 
 interface HomepageRegisterProps {
@@ -55,22 +67,98 @@ interface HomepageRegisterProps {
 }
 
 const Register: React.FC<HomepageRegisterProps> = ({background = false}) => {
+
+
+  const { value: usernameValue, bindings: {onChange: onChangeUsername} } = useInput("");
+  const { value: emailValue, bindings: {onChange: onChangeEmail} } = useInput("");
+
+  const validateEmail = (value: string) => {
+    return value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+  };
+
+  const helper: {
+    text: string;
+    color?: InputProps["helperColor"];
+  } = useMemo(() => {
+    if (!emailValue)
+      return {
+        text: "",
+        color: undefined,
+      };
+    const isValid = validateEmail(emailValue);
+    return {
+      text: isValid ? "Correct email" : "Enter a valid email",
+      color: isValid ? "success" : "error",
+    };
+  }, [emailValue]);
+
   return (
     <RegisterBackground id="register" css={{ $$backgroundImage: background ? 'url("../assets/images/register-bg.png")' : '' }}>
       <RegisterBox>
         <RegisterHeader>Sign up</RegisterHeader>
 
-        <RegisterLabel htmlFor="reg-username">Username</RegisterLabel>
-        <InputTransparent css={{marginBottom: '1rem'}}  type="text" name="reg-username" id="reg-username"/>
+        <FieldWrapper>
+          <RegisterLabel htmlFor="reg-username">Username</RegisterLabel>
+          <InputTransparent
+            onChange={onChangeUsername}
+            type="text"
+            name="reg-username"
+            id="reg-username"
+            fullWidth
+            clearable
+            required
+            shadow={false}
+            animated={false}
+            helperText="(3-32 characters, letters and underscores)"
+          />
+        </FieldWrapper>
 
-        <RegisterLabel htmlFor="reg-email">Email</RegisterLabel>
-        <InputTransparent css={{marginBottom: '1rem'}} type="text" name="reg-email" id="reg-email"/>
+        <FieldWrapper>
+          <RegisterLabel htmlFor="reg-email">Email</RegisterLabel>
+          <InputTransparent
+            onChange={onChangeEmail}
+            type="text"
+            name="reg-email"
+            id="reg-email"
+            fullWidth
+            clearable
+            required
+            shadow={false}
+            animated={false}
+            helperColor={helper.color}
+            helperText={helper.text}
+          />
+        </FieldWrapper>
 
-        <RegisterLabel htmlFor="reg-pass1">Password</RegisterLabel>
-        <InputTransparent css={{marginBottom: '1rem'}} type="password" name="reg-pass1" id="reg-pass1"/>
+        <FieldWrapper>
+          <RegisterLabel htmlFor="reg-pass1">Password</RegisterLabel>
+          <PasswordTransparent
+            type="password"
+            name="reg-pass1"
+            id="reg-pass1"
+            fullWidth
+            clearable
+            required
+            shadow={false}
+            animated={false}
+            helperText="(6 - 100 characters)"
+          />
+        </FieldWrapper>
 
-        <RegisterLabel htmlFor="reg-pass2">Password again</RegisterLabel>
-        <InputTransparent css={{marginBottom: '1rem'}} type="password" name="reg-pass2" id="reg-pass2"/>
+
+        <FieldWrapper>
+          <RegisterLabel htmlFor="reg-pass2">Password again</RegisterLabel>
+          <PasswordTransparent
+            type="password"
+            name="reg-pass2"
+            id="reg-pass2"
+            fullWidth
+            clearable
+            required
+            shadow={false}
+            animated={false}
+          />
+        </FieldWrapper>
 
         Are you ready to embark on an epic journey?
         <Button>Sign up</Button>
