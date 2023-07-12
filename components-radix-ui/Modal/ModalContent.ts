5b1a48fea18c3d13ef2aaa902c2671a56d0e5cef
Medo <martinmederly@gmail.com>
2023-07-12 11:32:52 +0200

    feat: modal window improvements - sizing and fixed close button

--- components-radix-ui/Modal/ModalContent.ts
+++ components-radix-ui/Modal/ModalContent.ts
@@ -1,5 +1,6 @@
 import * as DialogRadix from '@radix-ui/react-dialog';
 import { keyframes, styled } from '../../styles/stitches.config';
+import Stitches from '@stitches/react';
 
 const contentShow = keyframes({
   '0%': { opacity: 0, transform: 'translate(-50%, -48%) scale(.96)' },
@@ -22,4 +23,39 @@ export const ModalContent = styled(DialogRadix.Content, {
   zIndex: 10,
   animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
   '&:focus': { outline: 'none' },
+
+  variants: {
+    size: {
+      full: {
+        width: '90vw',
+        minWidth: '450px',
+      },
+      xs: {
+        width: '20vw',
+        minWidth: '200px',
+      },
+      sm: {
+        width: '30vw',
+        minWidth: '300px',
+      },
+      md: {
+        width: '40vw',
+        minWidth: '450px',
+      },
+      lg: {
+        width: '50vw',
+        minWidth: '450px',
+      },
+      xl: {
+        width: '60vw',
+        minWidth: '450px',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'full',
+  },
 });
+
+export type ModalSizeVariants = Stitches.VariantProps<typeof ModalContent>;
--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -3,32 +3,39 @@ import { ModalRoot } from '../../components-radix-ui/Modal/ModalRoot';
 import { ModalTrigger } from '../../components-radix-ui/Modal/ModalTrigger';
 import { ModalPortal } from '../../components-radix-ui/Modal/ModalPortal';
 import { ModalOverlay } from '../../components-radix-ui/Modal/ModalOverlay';
-import { ModalContent } from '../../components-radix-ui/Modal/ModalContent';
+import { ModalContent, ModalSizeVariants } from '../../components-radix-ui/Modal/ModalContent';
 import { ModalTitle } from '../../components-radix-ui/Modal/ModalTitle';
 import { ModalDescription } from '../../components-radix-ui/Modal/ModalDescription';
 import { ModalClose } from '../../components-radix-ui/Modal/ModalClose';
 import { MdClose } from 'react-icons/md';
 import { DialogProps } from '@radix-ui/react-dialog';
 
-interface ModalProps extends DialogProps {
+interface ModalProps extends DialogProps, ModalSizeVariants {
   trigger: React.ReactNode;
   content: React.ReactNode;
   title?: string;
   description?: string;
 }
 
-const Modal: React.FC<ModalProps> = ({ trigger, content, title, description, ...dialogProps }) => {
+const Modal: React.FC<ModalProps> = ({
+  trigger,
+  content,
+  title,
+  description,
+  size,
+  ...dialogProps
+}) => {
   return (
     <ModalRoot {...dialogProps}>
       <ModalTrigger asChild>{trigger}</ModalTrigger>
       <ModalPortal>
         <ModalOverlay />
-        <ModalContent onClick={(e) => e.stopPropagation()}>
+        <ModalContent size={size} onClick={(e) => e.stopPropagation()}>
           {title && <ModalTitle>{title}</ModalTitle>}
           {description && <ModalDescription>{description}</ModalDescription>}
           {content}
           <ModalClose asChild>
-            <MdClose />
+            <MdClose size={32} />
           </ModalClose>
         </ModalContent>
       </ModalPortal>
