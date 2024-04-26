49c3b61c5c2939ac8cf3dbb3f8ca4d18b77421df
Medo <martinmederly@gmail.com>
2024-04-26 08:22:21 +0200

    fix: fix changing of position in useUpdateMapLayer.ts

--- api/maps/useUpdateMapLayer.ts
+++ api/maps/useUpdateMapLayer.ts
@@ -13,12 +13,35 @@ export const useUpdateMapLayer = createMutation({
   mutationFn: async (variables: UpdateMapRequest) =>
     MapsCollection.mapsUpdateMapLayer(variables.mapId, variables.mapLayerId, variables.body),
   onSuccess: (data, variables) => {
-    const { mapId, mapLayerId } = variables;
+    const { mapId, mapLayerId, body } = variables;
     if (mapId) {
       const mapLayerQueryKey = useGetMapLayers.getKey(mapId);
-      queryClient.setQueryData<inferData<typeof useGetMapLayers>>(mapLayerQueryKey, (oldData) =>
-        (oldData ?? []).map((c) => (c.id === mapLayerId ? data.data : c)),
-      );
+      queryClient.setQueryData<inferData<typeof useGetMapLayers>>(mapLayerQueryKey, (od) => {
+        const oldData = od ?? [];
+        const startPosition = oldData.find((c) => c.id === mapLayerId)?.position ?? 0;
+
+        return oldData.map((c) => {
+          if (c.id === mapLayerId) return data.data;
+
+          const newPosition = body.position;
+          if (newPosition && startPosition > 0) {
+            const cPosition = c.position ?? 0;
+            let finalPosition = cPosition;
+            if (
+              cPosition >= Math.min(newPosition, startPosition) &&
+              cPosition <= Math.max(newPosition, startPosition)
+            ) {
+              if (startPosition < newPosition) {
+                finalPosition--;
+              } else if (startPosition > newPosition) {
+                finalPosition++;
+              }
+            }
+            return { ...c, position: finalPosition };
+          }
+          return c;
+        });
+      });
     }
   },
 });
