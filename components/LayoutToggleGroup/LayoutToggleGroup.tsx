dbc1ab9e5d71b29a2de5bc45a51466fa2dcea002
Medo <martinmederly@gmail.com>
2024-12-30 17:50:53 +0100

    feat: add LayoutToggleGroup component

---
+++ components/LayoutToggleGroup/LayoutToggleGroup.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ToggleGroupItem as ToggleGroupItems } from '../../components-radix-ui/ToggleGroup/toggleGroupLib';
+import { TbLayoutGrid, TbLayoutRows } from 'react-icons/tb';
+import ToggleGroup from '../ToggleGroup/ToggleGroup';
+import { LayoutToggleGroupOption } from './layoutToggleGroupLib';
+import { ToggleGroupSingleProps } from '@radix-ui/react-toggle-group';
+
+const layoutToggleGroupItems: ToggleGroupItems<LayoutToggleGroupOption>[] = [
+  {
+    label: 'Grid layout',
+    value: 'gridLayout',
+    icon: <TbLayoutGrid />,
+  },
+  {
+    label: 'Row layout',
+    value: 'rowLayout',
+    icon: <TbLayoutRows />,
+  },
+];
+
+type LayoutToggleGroupProps = Omit<
+  ToggleGroupSingleProps,
+  'type' | 'onValueChange' | 'defaultValue'
+> & {
+  onValueChange: (v: LayoutToggleGroupOption) => void;
+  defaultValue: LayoutToggleGroupOption;
+};
+
+const LayoutToggleGroup: React.FC<LayoutToggleGroupProps> = ({ ...toggleGroupProps }) => {
+  return <ToggleGroup type="single" items={layoutToggleGroupItems} {...toggleGroupProps} />;
+};
+
+export default LayoutToggleGroup;
---
+++ components/LayoutToggleGroup/layoutToggleGroupLib.ts
@@ -0,0 +1,1 @@
+export type LayoutToggleGroupOption = 'rowLayout' | 'gridLayout';
